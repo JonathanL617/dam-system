3e12cf5c9ccb831949283938575c714cb9adeccd
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-10 20:34:16 +0800

    Fix typo

--- frontend/lib/store.js
+++ frontend/lib/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import assetsReducer from "./slices/assetsSlice";
 
-cinst store = configureStore({
+const store = configureStore({
     reducer: {
         auth: authReducer,
         assets: assetsReducer,
