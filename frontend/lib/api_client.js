06729165384787d6f97f267cad9decfc0b6cf56e
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-10 23:13:34 +0800

    Add request interceptor

--- frontend/lib/api_client.js
+++ frontend/lib/api_client.js
@@ -3,3 +3,11 @@ import axios from "axios";
 const api = axios.create({
     baseURL: ProcessingInstruction.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
